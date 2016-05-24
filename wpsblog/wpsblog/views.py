944dfb0a55c727c2867acc8b4169e6d5d59a3036
feanar <feanar@feanar>
2016-05-24 17:22:22 +0900

    init production.txt add requests

--- wpsblog/wpsblog/views.py
+++ wpsblog/wpsblog/views.py
@@ -1,8 +1,15 @@
+import requests
+
 from django.http.response import HttpResponse
 
 def home(request):
     return HttpResponse("hello world")
 
 def room(request, room_id):
-    return HttpResponse("This is a room detail " + room_id)
+    url = "http://api.zigbang.com/v1/items?detail=true&item_ids=" + room_id
+    response = requests.get(url)
+    return HttpResponse(
+        response.content,
+        content_type="application/json",
+    )
 
