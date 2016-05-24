90a068d3377db037301a90151edec4409836ef8e
feanar <feanar@feanar>
2016-05-24 16:43:30 +0900

    init urls.py add url

--- wpsblog/wpsblog/urls.py
+++ wpsblog/wpsblog/urls.py
@@ -1,21 +1,22 @@
-"""wpsblog URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.9/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf.urls import url
 from django.contrib import admin
 
+from django.http.response import HttpResponse
+
+# MVC
+# M_Model: DB ( Data ) & Business Logic ( * )
+# V_ViewL HTML, CSS, .. -> Templatae/Client
+# C_Controller: View, Model..
+
+# MVC 제작의 가장 좋은 프로세스 개발은 아래와 같이 한다.
+# Model -> 더 무겁게
+# Controller -> 더 가볍게 ( 즉, 기능이 Controller -> Model..)
+
+def home(request):
+    return HTTPResponse("hello world")
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+
+    url(r'^$', home),
 ]
