3aa2665a734690121da8dab611a68923d7e944c0
feanar <feanar@feanar>
2016-05-25 13:31:45 +0900

    views.py add def news in watcha

--- wpsblog/wpsblog/views.py
+++ wpsblog/wpsblog/views.py
@@ -15,3 +15,30 @@ def room(request, room_id):
         content_type="application/json",
     )
 
+def news(request):
+   search = request.GET.get("search") # title 에 search 가 포함되어 있는가? 
+   
+   response = resquests.get("https://watcha.net/home/news.json?page=1&per=50")
+   news_dict = response.json() #-> requests.json 
+   
+   #news_items_list = news_dict.get("news")     
+   #from IPython import embed; embed()
+
+   content = "<h1>News</h1>" +\
+             "<p>This is news page.</p>" +\
+             "".join([
+             	"<h2>{title}</h2><img src={image_src}<p>{content}</p>".format(
+             		title = news.get('title'),
+                	image_src = news.get('image'),
+			content = news.get('content'),
+		)
+             	for news
+             	in news_dict["news"]
+             
+             ])
+
+   return HttpResponse(
+      #response.content
+      #content_type="application/json" #watcha view
+      content
+   )
