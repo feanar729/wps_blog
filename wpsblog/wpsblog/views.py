8407113622613d0c31606cb7bb7f759374f14a4e
feanar729 <feanar729@gmail.com>
2016-05-30 01:14:50 +0900

    modified views.py & home.html: refactoring and deburging code

--- wpsblog/wpsblog/views.py
+++ wpsblog/wpsblog/views.py
@@ -20,20 +20,20 @@ def room(request, room_id):
     )
 
 def news(request):
-   search = request.GET.get("search") # title 에 search 가 포함되어 있는가? 
+    search = request.GET.get("search") # title 에 search 가 포함되어 있는가? 
    
-   response = resquests.get("https://watcha.net/home/news.json?page=1&per=50")
-   news_dict = json.loads(response.text)
-   news_list = news_dict.get("news")
+    response = requests.get("https://watcha.net/home/news.json?page=1&per=50")
+    news_dict = json.loads(response.text)
+    news_list = news_dict.get("news")
 
-   if search:
-       news_list = list(filter(
-           lambda news: search in news.get('title'),
-           news_list,
-       ))
+    if search:
+        news_list = list(filter(
+            lambda news: search in news.get('title'),
+            news_list,
+        ))
    
-   return render(
-        request,
-        "news.html",
-        {"news_list": news_list},
-   )
+    return render(
+         request,
+         "news.html",
+         {"news_list": news_list},
+    )
