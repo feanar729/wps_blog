505d299a5632d1f58b02007a28c26d9239073b7b
feanar729 <feanar729@gmail.com>
2016-05-29 17:08:19 +0900

    update wpsblog/urls.py: add sentence written MVVM, MVW

--- wpsblog/wpsblog/urls.py
+++ wpsblog/wpsblog/urls.py
@@ -8,6 +8,11 @@ from wpsblog.views import home, room
 # V_ViewL HTML, CSS, .. -> Templatae/Client
 # C_Controller: View, Model..
 
+# 이밖에 패턴들...
+# MVVM => Model View View Model
+# MVW => Model View Whatever
+
+
 # MVC 제작의 가장 좋은 프로세스 개발은 아래와 같이 한다.
 # Model -> 더 무겁게
 # Controller -> 더 가볍게 ( 즉, 기능이 Controller -> Model..)
