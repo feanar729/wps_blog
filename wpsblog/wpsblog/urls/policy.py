4b43287024bae76bbe75f8ed2868b5966a901729
feanar729 <feanar729@gmail.com>
2016-05-30 15:44:04 +0900

    modified policy.py: write order lesson flow.

--- wpsblog/wpsblog/urls/policy.py
+++ wpsblog/wpsblog/urls/policy.py
@@ -8,3 +8,8 @@ urlpatterns = [
     url(r'privacy/$', privacy, name="privacy"),
     url(r'disclaimer/$', disclaimer, name="disclaimer"),
     ]
+
+# 1. policy_urlpatterns 를 변수로
+# 2. policy_urlpatterns 를 파일로 ( from .. import )
+# 3. policy_urls 라는 모듈로 ( include 가 모듈 이름을 받을 수 있었다.)
+# 4. urls 
