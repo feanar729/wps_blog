c3f1f99dc40e3b179612217461c241bebf3507fc
feanar729 <feanar729@gmail.com>
2016-06-15 15:39:57 +0900

    update .travis.yml: explicit testing on travis and fix.

--- wpsblog/wpsblog/tests.py
+++ wpsblog/wpsblog/tests.py
@@ -1,19 +1,26 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+
+from bitly.models.bitlink import Bitlink
 
 
-class PricingViewTestCase(TestCase):
+class BitlinkModelTestCase(TestCase):
 
-    def setUp(self):
-        self.response = self.client.get("/pricing/")
-
-    def test_pricing_view_should_return_200(self):
-        self.assertEqual(
-            self.response.status_code,
-            200,
+    def test_bitlink_should_have_shorten_hash(self):
+        # Create test user
+        test_username = "test_username"
+        test_password = "test_password"
+        test_user = User.objects.create_user(
+            username=test_username,
+            password=test_password,
         )
 
-    def test_pricing_view_should_have_good_title(self):
-        self.assertContains(
-            self.response,
-            "Pricing Table",
+        # Create test bitlink
+        test_original_url = "http://www.example.com"
+        bitlink = Bitlink.objects.create(
+            user=test_user,
+            original_url=test_original_url,
+        )
+        self.assertTrue(
+            bitlink.shorten_hash,
         )
